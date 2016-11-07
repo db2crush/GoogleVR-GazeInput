116a91ad73dfb0276e57f88aae127cbfb143ffc9
SangGee <erunningo@gmail.com>
2016-11-07 14:57:45 -0500

    GazeInput
    GazeInput by Script

---
+++ Assets/DetectGaze.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DetectGaze : MonoBehaviour, IGvrGazeResponder
+{
+    public void OnGazeEnter()
+    {
+        Debug.Log("Gaze Entered!!!");
+    }
+
+    public void OnGazeExit()
+    {
+
+    }
+
+    public void OnGazeTrigger()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
